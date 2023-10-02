10e9af9e34ba43e2a3f85038f62d9d6c1a3b9cc5
Knutson <aknutso4@LAMU0KX0P997NVJ.uhc.com>
2023-10-02 08:17:58 -0500

    added pseduo code comments

--- simple-aged-cache/src/main/java/io/collective/SimpleAgedCache.java
+++ simple-aged-cache/src/main/java/io/collective/SimpleAgedCache.java
@@ -4,20 +4,31 @@ import java.time.Clock;
 
 public class SimpleAgedCache {
 
+    public class ExpirableEntry {
+        public ExpirableEntry() {
+            //need to define variables
+        }
+    }
+
     public SimpleAgedCache(Clock clock) {
+        //look up constrcutor chaining
     }
 
     public SimpleAgedCache() {
+        //need to initiate an array
     }
 
     public void put(Object key, Object value, int retentionInMillis) {
+        //create ExpirableEntry and add to array
     }
 
     public boolean isEmpty() {
+        // check ExpirableEntry to see if anything needs to be expiered, then return True if count > 0
         return false;
     }
 
     public int size() {
+        // check ExpirableEntry to see if anything needs to be expired, then return length of array 
         return 0;
     }
 
